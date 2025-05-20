c253680b53ded1d949529cf93fa9c9db2d67b228
Long Duong <duongthanhlong220805@gmail.com>
2025-05-20 14:45:36 +0700

    update src folder

---
+++ SmartRide/SmarRide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmarRide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
