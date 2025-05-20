944209b848ccb69205b6623b64b7882745494a0a
Long Duong <duongthanhlong220805@gmail.com>
2025-05-20 14:58:03 +0700

    update src folder

---
+++ SmartRide/data/smartRideDB.sql
@@ -0,0 +1,31 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'SmartRide')
+BEGIN
+    CREATE DATABASE SmartRide;
+END
+
+GO
+
+USE SmartRide;
+
+-- Create Role table
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Role')
+BEGIN
+CREATE TABLE Role (
+    role_id INT PRIMARY KEY,
+    role_name VARCHAR(30)
+);
+END
+
+-- Create Account table
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Account')
+BEGIN
+CREATE TABLE Account (
+    accountId UNIQUEIDENTIFIER PRIMARY KEY,
+    username NVARCHAR(50),
+    password NVARCHAR(50),
+    hide BIT,
+    passwordResetToken VARCHAR(max),
+    tokenExpiration VARCHAR(max),
+    role_id INT
+);
+END
